6bb02f69fb8e22221b790a3b52a5028e524abd5b
OlgaBaida <109526437+OlgaBaida@users.noreply.github.com>
2023-01-03 10:57:46 +0200

    додано друге завдання

--- js/task-02.js
+++ js/task-02.js
@@ -6,4 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients');
+
+const newList = ingredients.map(newEl => 
+  {let elementLi = document.createElement("li");
+    elementLi.className = "item";
+    elementLi.textContent = newEl;
+    return (elementLi)});
+
+list.append(...newList);
+console.log (...newList)
+
+
 
