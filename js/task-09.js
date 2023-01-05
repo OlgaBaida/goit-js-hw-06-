31c3ff1429ee7d7a89e3e622f747367dba6215b8
OlgaBaida <109526437+OlgaBaida@users.noreply.github.com>
2023-01-05 15:06:57 +0200

    додано завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const changeButton = document.querySelector('button.change-color')
-const body = document.querySelector('body')
+const changeBtn = document.querySelector('button.change-color')
+const bodyEl = document.querySelector('body')
 const colorValue = document.querySelector('span.color')
 
 
-changeButton.addEventListener('click', e => {
+changeBtn.addEventListener('click', e => {
   let color = getRandomHexColor();
-  body.style.backgroundColor = color;
+  bodyEl.style.backgroundColor = color;
   colorValue.textContent = color;
 })
